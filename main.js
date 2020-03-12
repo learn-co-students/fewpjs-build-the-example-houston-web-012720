709c82477c56100004f94d62f9b8c0c7a1ee0669
anamsoomro <anamsoomroed@gmail.com>
2020-03-12 17:58:06 -0500

    Done.

--- main.js
+++ main.js
@@ -8,6 +8,31 @@ let error = document.querySelector("#modal")
 
 error.className = "hidden"
 
+const hearts = document.querySelectorAll(".like-glyph")
+hearts.forEach((heart) => 
+{heart.addEventListener("click",()=> {
+  mimicServerCall()
+  .then (()=>{
+    // heart.innerText = FULL_HEART
+    // heart.className = "activated-heart"
+    if (heart.innerText == FULL_HEART) {
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove("activated-heart")
+    } else if (heart.innerText == EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.className = "activated-heart"
+    }  
+  })
+  .catch( message => {
+    error.classList.remove("hidden")
+    error.innerText = message
+    setTimeout(function(){
+      error.className = "hidden"
+    },5000)
+  })
+})}
+)
+
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
